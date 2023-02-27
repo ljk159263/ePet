471c7ddd550c3b510008f9c05a6042d517f8118c
ljk <ljk159263@naver.com>
2023-02-27 11:46:48 +0900

    카카오지도 연동

--- app/src/main/java/com/example/epet/LoginActivity.java
+++ app/src/main/java/com/example/epet/LoginActivity.java
@@ -1,7 +1,5 @@
 package com.example.epet;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -120,7 +120,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
+
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
         }
     }
+
+
 }
 
